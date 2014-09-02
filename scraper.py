afd46d8301c643a855ec187e7dce725b172ffd0c
Molly Long <mlong@pinterest.com>
2014-09-02 01:13:11 -0700

    yay workin

--- scraper.py
+++ scraper.py
@@ -1,6 +1,14 @@
+#!/usr/bin/python -tt
+# -*- coding: utf-8 -*-
+
 import flickrapi
 import re
 import json
+import codecs
+import sys
+
+UTF8Writer = codecs.getwriter('utf8')
+sys.stdout = UTF8Writer(sys.stdout)
 
 # api_key = '6b30d24a901279dcbabac8e4a438d155'
 api_secret = '49dd1aeef48fcece'
@@ -9,7 +17,7 @@ api_key = '47dbc3b01589b715b9c204211bcda208'
 api_secret = '4df9fe589fb912d4'
 
 flickr = flickrapi.FlickrAPI(api_key, api_secret)
-photos = flickr.photos_search(text='San Francisco Food', per_page='100', format="json")
+photos = flickr.photos_search(text='San Francisco Food', in_gallery='true', per_page='50', format="json")
 response_parser = re.compile(r'jsonFlickrApi\((.*?)\)$')
 parsed_photos = response_parser.findall(photos)
 photos = json.loads(parsed_photos[0])
@@ -17,6 +25,7 @@ photos_list = photos['photos']['photo']
 final_photos_list = []
 # print len(photos_list)
 
+print '['
 for photo in photos_list:
 	currId = photo['id']
 	currTitle = photo['title']
@@ -24,12 +33,9 @@ for photo in photos_list:
 	response_parser = re.compile(r'jsonFlickrApi\((.*?)\)$')
 	parsedPhoto = response_parser.findall(currphoto)
 	currphoto = json.loads(parsedPhoto[0])
-	# import pdb; pdb.set_trace()
 	currphoto_sizes = currphoto['sizes']['size']
-	# print currphoto
+	
 	for size in currphoto_sizes:
 		if size['label'] == "Large":
-			final_photos_list.append((currId, currTitle, size['source']))
-			# print size['source']
-
-print final_photos_list
+			print '{"id":"' + currId + '", "title":"' + currTitle + '", "source":"' + size['source']+'"},'
+print ']'
--- server.py
+++ server.py
@@ -3,15 +3,9 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 import random
 from random import randint
+import json
 
-files = ["http://tastethestyle.files.wordpress.com/2013/08/lobster.jpg", "http://foodforfel.com/wp-content/gallery/sushiyasaka.5.12.12/IMG_6651.jpg",
-	"http://cantabriaenboca.files.wordpress.com/2013/03/nigiri.jpg", "http://4.bp.blogspot.com/-6kDkkvq0JrA/UNyn3WZle-I/AAAAAAAABJg/m362Mg2I-m4/s1600/dumplings2.JPG",
-	"http://valeryscafe.com/sites/default/files/imagecache/product_full/Banana-Pancakes.jpg", "http://www.dirtylaundrykitchen.com/wp-content/uploads/2014/03/challah-french-toast-with-berries.jpg",
-	"http://blog.centralrestaurant.com/wp-content/uploads/WW-SantaCruzGarlicFries-Up.jpg", "http://outoftheordinaryfood.files.wordpress.com/2012/04/pizza-dumplings1.jpg",
-	"http://cynthesizingfood.files.wordpress.com/2012/08/dsc_0116.jpg", "http://makingsundaysauce.files.wordpress.com/2011/10/mg_0959.jpg",
-	"http://24.media.tumblr.com/62a90d23a554d12e39bdd91b374d9cfb/tumblr_mxv2lsnOKK1rcvl68o2_1280.jpg",
-	"http://1.bp.blogspot.com/-rvNqkZBZ6D8/UhvHX4o09xI/AAAAAAAAJhU/2ELNVDQ-98g/s1600/P8263537.JPG", "http://4.bp.blogspot.com/-97dGsPlCi5M/TWMmcFVxShI/AAAAAAAAAJc/8UZuIdAfjuQ/s1600/chocolate_purple_macaron.jpg",
-	"http://seonkyounglongest.com/wp-content/uploads/2013/05/IMG_1199.jpg", "http://thevegetarianginger.files.wordpress.com/2012/11/dsc0566.jpg"]
+data = []
 	
 class myHandler(BaseHTTPRequestHandler):
 	
@@ -20,20 +14,26 @@ class myHandler(BaseHTTPRequestHandler):
 		self.send_response(200)
 		self.send_header('Content-type','text/html')
 		self.end_headers()
-		if len(files) > 0:
-			index = random.randint(0,len(files)-1)
-			toSend = files[index]
-			del files[index]
+		if len(data) > 0:
+			index = random.randint(0,len(data)-1)
+			toSend = data[index]['source']
+			# del files[index]
 			print toSend
 			self.wfile.write(toSend)
 		else:
 			self.wfile.write('None')
 		return
 
+def load_data():
+	json_data=open('temp.txt')
+	global data
+	data = json.load(json_data)
+	json_data.close()
 
 def main():
 	try:
 		server = HTTPServer(('', 8080), myHandler)
+		load_data()
 		server.serve_forever()
 	except KeyboardInterrupt:
 		print '^C recieved'
