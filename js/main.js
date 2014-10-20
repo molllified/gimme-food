6a69de8e320b998060b8e0a2ac8da90896da608c
Molly Long <molllified@gmail.com>
2014-10-20 00:35:28 -0700

    right key and handling dates

--- js/main.js
+++ js/main.js
@@ -3,21 +3,67 @@ var counter = 1;
 var numToLoad= 10;
 var skrollr;
 var getImages= true;
+var MONTHS_CONST = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var DAYS_CONST = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('clock').innerHTML = h+":"+m+":"+s;
-    var t = setTimeout(function(){
-      startTime()
-    }, 500);
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: // left
+        break;
+
+        case 38: // up
+        break;
+
+        case 39: // right
+          console.log('right');
+          newImage();
+        break;
+
+        case 40: // down
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
+
+function startDate() {
+  var today = new Date();
+  var date = today.getDate();
+  var month = MONTHS_CONST[today.getMonth()];
+  var day = DAYS_CONST[today.getDay()];
+
+  document.getElementById('datetime').innerHTML = day+', '+month+ ' '+date;
 }
 
-function checkTime(i) {
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  h = handleHour(h)
+  m = addZero(m);
+  s = addZero(s);
+  document.getElementById('clock').innerHTML = h+":"+m+":"+s;
+
+  var t = setTimeout(function(){
+    startTime()
+  }, 500);
+}
+
+function handleHour(hour) {
+  if (hour < 12) {
+    if (hour == 0) {
+      return 12;
+    }
+    return hour;
+  }
+  else {
+    hour - 12;
+  }
+}
+
+function addZero(i) {
     if (i<10) {
       i = "0" + i
     };  
@@ -60,6 +106,7 @@ function loadImages(number){
 $(document).ready(function() {
 	loadImages();
   startTime();
+  startDate();
   var next = document.getElementById('next');
   next.onclick = newImage;
 })
