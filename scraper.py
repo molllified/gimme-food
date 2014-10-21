1c002a4be9bb9589fdaf640e476e596e8c3eac13
Molly Long <molllified@gmail.com>
2014-10-21 01:22:21 -0700

    modify query so better pix

--- scraper.py
+++ scraper.py
@@ -17,7 +17,8 @@ api_key = '47dbc3b01589b715b9c204211bcda208'
 api_secret = '4df9fe589fb912d4'
 
 flickr = flickrapi.FlickrAPI(api_key, api_secret)
-photos = flickr.photos_search(text='san francisco food', per_page='50', sort='relevance', has_geo='true', format='json', extras='geo, url_o')
+photos = flickr.photos_search(text='san francisco food', per_page='500', sort='relevance', has_geo='true', format='json', media='photos', 
+	extras='geo, url_o, o_dims')
 response_parser = re.compile(r'jsonFlickrApi\((.*?)\)$')
 parsed_photos = response_parser.findall(photos)
 photos = json.loads(parsed_photos[0])
@@ -25,11 +26,16 @@ photos_list = photos['photos']['photo']
 
 print '['
 for photo in photos_list:
+	if 'height_o' not in photo or int(photo['height_o']) < 1000 or float(photo['height_o'])/float(photo['width_o']) >1.25:
+		continue
 	currId = photo['id']
 	currTitle = photo['title']
 	if 'url_o' in photo:
 		source = photo['url_o']
 	else:
 		source = "https://farm{0}.staticflickr.com/{1}/{2}_{3}.jpg".format(photo['farm'], photo['server'], photo['id'], photo['secret'])
+	# if photo['url_o'] == 'https://farm5.staticflickr.com/4077/4887555902_f8eb4d63ab_o.jpg':
+		# import pdb; pdb.set_trace()
+
 	print '{"id":"' + currId + '", "title":"' + currTitle + '", "source":"' + source +'", "longitude":"' + str(photo['longitude']) + '", "latitude":"' + str(photo['latitude']) + '"},'
 print ']'
